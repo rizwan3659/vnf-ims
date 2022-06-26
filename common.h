3aa9fb30f1bc2c4f299825f426724618eb598ef2
rizwan alam <riscogian@gmail.com>
2022-06-26 12:54:04 +0530

    Add files via upload

---
+++ common.h
@@ -0,0 +1,24 @@
+/*
+This file contains list of addresses which are used by all the components.
+*/
+
+#define MAXCONN 10000
+#define MAXEVENTS 10000
+
+/*
+PCSCFPORTNO is port number on which PCSCF listens. 
+*/
+#define PCSCFPORTNO 5060
+#define ICSCFPORTNO 5060
+#define SCSCFPORTNO 5060
+#define HSSPORTNO 5060
+
+/*
+PCSCFADDR is IP address of P-CSCF. This address will be used by P-CSCF to bind along with PCSCFPORTNO. 
+*/
+
+#define UEADDR "192.168.179.225"
+#define PCSCFADDR "192.168.179.196"
+#define ICSCFADDR "192.168.179.197"
+#define SCSCFADDR "192.168.179.67"
+#define HSSADDR "192.168.179.66"
